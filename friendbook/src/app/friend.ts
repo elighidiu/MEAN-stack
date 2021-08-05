7f82a8db85f132b06b0c4245f5b7392ca0fbd373
Your Nameeli <elighidiu@yahoo.com>
2021-08-05 16:11:10 +0200

    angular project created

---
+++ friendbook/src/app/friend.ts
@@ -0,0 +1,8 @@
+export class Friend {
+     constructor(public lastname: string,
+                    public firstname: string,
+                    public email:string, 
+                    public phone:string, 
+                    public language:string, 
+    ){ }
+}
